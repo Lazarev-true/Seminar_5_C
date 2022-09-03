152eb17b5ea108ca1950b9c4682acb47a8cb702f
Lazarev Konstantin <klik@example.com>
2022-09-03 09:22:26 +0300

    Сумма элементов

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму четных 
+// элементов, стоящих на нечётных позициях 
+// (позиция - индекс элемента в массиве).
+
+Random x = new Random();
+
+int N = 10;
+int[] array = new int[N];
+int sum = 0;
+
+for (int i = 0; i < N; i++)
+{
+   array[i] = x.Next(1, 100);
+   if((array[i] % 2 == 0) && (i % 2 != 0))
+   {
+        sum += array[i];
+   }
+}
+
+var str = string.Join(" ", array);
+Console.WriteLine($"[{str}] -> {sum}");

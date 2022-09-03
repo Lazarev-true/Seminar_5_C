1aeb53c38e5a68ccb909751fdeede16d9cc64678
Lazarev Konstantin <klik@example.com>
2022-09-03 09:16:42 +0300

    Чётные числа в массиве

---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет 
+// количество чётных чисел в массиве.
+
+
+Console.WriteLine("Введите размер массива");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Random x = new Random();
+
+int[] array = new int[N];
+
+int volume = 0;
+
+for (int i = 0; i < N; i++)
+{
+   array[i] = x.Next(100, 1000);
+   if(array[i] % 2 == 0)
+   {
+        volume += 1;
+   }
+}
+var str = string.Join(" ", array);
+Console.WriteLine($"[{str}] -> {volume}");
